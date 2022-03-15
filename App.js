ea5e06c84d04c471507e35abf05224a0fc5c7b78
charles <charlesrabeclegloahec@gmail.com>
2022-03-15 14:03:25 +0100

    ajout d'un bouton de recherche

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Search from './Components/Search'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello !!!! </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return <Search />
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
---
+++ Components/Search.js
@@ -0,0 +1,27 @@
+// Components/Search.js
+import React from 'react'
+import { View, TextInput, Button } from 'react-native'
+
+class Search extends React.Component {
+    render() {
+        return (
+            <View style={{ marginTop: 20 }}>
+                <TextInput placeholder='Titre du film' 
+                  style={{
+                    marginLeft: 15,
+                    marginRight: 15,
+                    marginBottom: 20,
+                    height: 50,
+                    borderColor: '#000000',
+                    borderWidth: 1,
+                    paddingLeft: 5,
+                  }}
+                  />
+                <Button title='Rechercher' onPress={() => { }}/>
+            </View>
+        )
+    }
+}
+
+export default Search
+
