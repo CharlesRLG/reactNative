40e6579ae4bf55f3335e1d4ba3682b40a2851e15
charles <charlesrabeclegloahec@gmail.com>
2022-03-15 16:14:47 +0100

    mises en place des film

---
+++ Components/FilmItem.js
@@ -0,0 +1,17 @@
+// Components/FilmItem.js
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class FilmItem extends React.Component {
+  render() {
+    const film = this.props.film
+    console.log(this.props.film.title)
+    return (
+      <View>
+        <Text>{film.title}</Text>
+      </View>
+    )
+  }
+}
+
+export default FilmItem
--- Components/Search.js
+++ Components/Search.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { View, TextInput, Button, StyleSheet} from 'react-native'
 import { FlatList, Text } from 'react-native'
+import films from '../Helpers/filmsData'
+import FilmItem from './FilmItem'
 
 const styles = StyleSheet.create({
     main_container: {
@@ -34,10 +36,8 @@ class Search extends React.Component {
             <View style={styles.main_container}>
                 <TextInput style={styles.textinput} placeholder='Titre du film'/>
                 <Button title='Rechercher' onPress={() => { }}/>
-                <FlatList 
-                data={ [ {key: 'a'}, {key: 'b'}]}
-                renderItem={({item}) => <Text>{item.key}</Text>}>
-                </FlatList>
+                <FlatList data={films} renderItem={({ item }) => <FilmItem film={item} />} 
+                keyExtractor={(item) => item.id.toString()}/>
             </View>
         )
     }
