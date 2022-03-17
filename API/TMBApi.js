be30c16c84ab7b8b49cf6f50eee81edb16df78fc
charles <charlesrabeclegloahec@gmail.com>
2022-03-17 13:45:45 +0100

    ajout API

---
+++ API/TMBApi.js
@@ -0,0 +1,13 @@
+// API/TMDBApi.js
+import axios from 'axios'
+import { API_TOKEN } from '@env'
+
+const getFilmsFromApiWithSearchedText = async (text) => {
+  const url =
+    'https://api.themoviedb.org/3/search/movie?api_key=' +
+    API_TOKEN +
+    '&language=fr&query=' +
+    text
+}
+
+export default getFilmsFromApiWithSearchedText
